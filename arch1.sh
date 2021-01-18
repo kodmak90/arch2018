ce6119817b331c4a13a7ae2aac2a5682eff158d4
kodmak90 <66167986+kodmak90@users.noreply.github.com>
2021-01-18 03:28:46 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -75,4 +75,3 @@ pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
